757108e9d27a0ca0736c1b7022901e52ae072ea6
Lev Orlov <lev.orlov.dev@gmail.com>
2024-01-21 14:28:04 -0300

    Add some examples

--- 6_guads.ts
+++ 6_guads.ts
@@ -1,1 +1,39 @@
-// на 53 минуте остановился тут: https://www.youtube.com/watch?v=nyIpDs2DJ_c
+function strip(x: string | number) {
+    if (typeof x === 'number') {
+        return x.toFixed(2)
+    }
+    return x.trim()
+}
+
+class MyResponse {
+    header = 'response header'
+    result = 'response result'
+}
+
+class MyError {
+    header = 'error header'
+    message = 'error message'
+}
+
+function handle(resp: MyResponse | MyError) {
+    if (resp instanceof MyResponse) {
+        return {
+            info: resp.header + resp.result
+        }
+    }
+    return {
+        info: resp.header + resp.message
+    }
+}
+
+// ======================
+
+type AlertType = 'success' | 'danger' | 'warning'
+
+function setAlertType(type: AlertType) {
+    // ...
+}
+
+setAlertType('success')
+setAlertType('warning')
+// setAlertType('default')
